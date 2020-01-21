de43e56d51b76ffd993bcc2a97f0956fa6062b99
lpiem <lpiem@iutabgdin202-02.univ-lyon1.fr>
2020-01-21 11:07:20 +0100

    Add router and nnavigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,27 @@
-import React from 'react';
-import './App.css';
-import './characterContainer.css'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-type Info = {
-  count: number
-  pages: number
-}
+import Characters from './Characters';
+import Character from './Character';
 
-type Characters = {
-  info: Info
-  results: Array<{
-    id: number
-    name: string
-    image: string
-  }>
-}
-
-const getCharacters = (page = 1) =>
-  fetch(`https://rickandmortyapi.com/api/character/?page=${page}`, {
-    headers: { Accept: 'application/json' },
-  }).then<Characters>(res => res.json())
-
-const App: React.FC = () => {
-  const [characters, setCharacters] = React.useState<Characters| null>(null)
-  const [loading, setLoading] = React.useState(false)
-  const [page, setPage] = React.useState(1)
-
-  React.useEffect(() => {
-    let cancel = false
-    setLoading(true)
-
-    getCharacters(page).then(data => {
-      if (!cancel) {
-        setCharacters(data)
-        setLoading(false)
-      }
-    })
-
-    return () => {
-      cancel = true
-    }
-  }, [page])
-
+export default function App() {
   return (
-      <div className="App">
-        <div className="divButtonStyle">
-          <button disabled={loading} onClick={() => setPage(page - 1)}>
-            Previous
-          </button>
-          <button disabled={loading} onClick={() => setPage(page + 1)}>
-            Next
-          </button>
-        </div>
-        {loading ? (
-          <p className="loading">Loading...</p>
-        ) : (
-          characters && characters.results.map(character => <div key={character.id} className="characterContainer" onClick={() => setPage(page + 1)}>
-            <img className="imgStyle" src={character.image} alt="No image" width="40" height="40" />
-            <span className="spanStyle">{character.name}</span></div>
-          )
-        )}
-        <div className="divButtonStyle">
-          <button disabled={loading} onClick={() => setPage(page - 1)}>
-            Previous
-          </button>
-          <button disabled={loading} onClick={() => setPage(page + 1)}>
-            Next
-          </button>
-        </div>
-      </div>
-    )
+    <Router>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/characters">
+            <Characters />
+          </Route>
+          <Route 
+            path={"/character/:characterId"}
+            component={Character} />
+        </Switch>
+    </Router>
+  );
 }
-
-export default App;
