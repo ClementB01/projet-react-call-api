478d15d2b20d9fa05273017a6931b492ab73d11a
lpiem <lpiem@iutabgdin202-03.univ-lyon1.fr>
2020-01-21 13:56:13 +0100

    lock min and max page

--- src/Characters.tsx
+++ src/Characters.tsx
@@ -1,80 +1,122 @@
-import React from 'react';
-import './Character';
-import './characterContainer.css';
-import './Characters.css';
+import React from "react";
+import "./Character";
+import "./characterContainer.css";
+import "./Characters.css";
 
 import {
-    Link
-  } from "react-router-dom";
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+  Redirect
+} from "react-router-dom";
 
 type Info = {
-  count: number
-  pages: number
-}
+  count: number;
+  pages: number;
+};
 
 type Characters = {
-  info: Info
+  info: Info;
   results: Array<{
-    id: number
-    name: string
-    image: string
-  }>
-}
+    id: number;
+    name: string;
+    image: string;
+  }>;
+};
 
-const getCharacters = (page = 1) =>
+const getCharacters = (page: number | string) =>
   fetch(`https://rickandmortyapi.com/api/character/?page=${page}`, {
-    headers: { Accept: 'application/json' },
-  }).then<Characters>(res => res.json())
+    headers: { Accept: "application/json" }
+  }).then<Characters>(res => res.json());
 
 const App: React.FC = () => {
-  const [characters, setCharacters] = React.useState<Characters| null>(null)
-  const [loading, setLoading] = React.useState(false)
-  const [page, setPage] = React.useState(1)
+  const [characters, setCharacters] = React.useState<Characters | null>(null);
+  const [loading, setLoading] = React.useState(false);
+  //const [page, setPage] = React.useState(1);
+
+  let p = 1;
+  let { numPage } = useParams();
+  if (numPage) {
+    p = Number(numPage);
+  }
 
   React.useEffect(() => {
-    let cancel = false
-    setLoading(true)
+    let cancel = false;
+    setLoading(true);
 
-    getCharacters(page).then(data => {
-      if (!cancel) {
-        setCharacters(data)
-        setLoading(false)
-      }
-    })
+    if (p) {
+      getCharacters(p).then(data => {
+        if (!cancel) {
+          setCharacters(data);
+          setLoading(false);
+        }
+      });
+    }
 
     return () => {
-      cancel = true
-    }
-  }, [page])
+      cancel = true;
+    };
+  }, [p]);
+
+  let buttonNext;
+  if (p < 25) {
+    buttonNext = (
+      <Link to={`/${p + 1}`}>
+        <button disabled={loading}>Next</button>
+      </Link>
+    );
+  } else {
+    buttonNext = <button disabled={true}>Next</button>;
+  }
+
+  let buttonPrevious;
+  if (p > 1) {
+    buttonPrevious = (
+      <Link to={`/${p - 1}`}>
+        <button disabled={loading}>Previous</button>
+      </Link>
+    );
+  } else {
+    buttonPrevious = <button disabled={true}>Previous</button>;
+  }
 
   return (
-      <div className="App">
-        <div className="divButtonStyle">
-          <button disabled={loading} onClick={() => setPage(page - 1)}>
-            Previous
-          </button>
-          <button disabled={loading} onClick={() => setPage(page + 1)}>
-            Next
-          </button>
-        </div>
-        {loading ? (
-          <p className="loading">Loading...</p>
-        ) : (
-          characters && characters.results.map(character => <Link to={`/character/${character.id}`} className='linkWithoutTextDecoration'><div key={character.id} className="characterContainer">
-            <img className="imgStyle" src={character.image} alt="No image" width="40" height="40" />
-            <span className="spanStyle">{character.name}</span></div></Link>
-          )
-        )}
-        <div className="divButtonStyle">
-          <button disabled={loading} onClick={() => setPage(page - 1)}>
-            Previous
-          </button>
-          <button disabled={loading} onClick={() => setPage(page + 1)}>
-            Next
-          </button>
-        </div>
+    <div className="App">
+      <div className="divButtonStyle">
+        {buttonPrevious}
+        {buttonNext}
       </div>
-    )
-}
+      {loading ? (
+        <p className="loading">Loading...</p>
+      ) : (
+        characters &&
+        characters.results.map(character => (
+          <Link
+            to={`/character/${character.id}`}
+            className="linkWithoutTextDecoration"
+          >
+            <div key={character.id} className="characterContainer">
+              <img
+                className="imgStyle"
+                src={character.image}
+                alt="No image"
+                width="40"
+                height="40"
+              />
+              <span className="spanStyle">{character.name}</span>
+            </div>
+          </Link>
+        ))
+      )}
+      <div className="divButtonStyle">
+        {buttonPrevious}
+        {buttonNext}
+      </div>
+    </div>
+  );
+};
 
 export default App;
